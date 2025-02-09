eb58d7786964b1e941fa8df83c6671e95b150299
CarlosMi11 <carlosmirandaucv@gmail.com>
2025-02-09 14:42:08 -0400

    MAVEN PROJECT

---
+++ src/FacUpdate/src/main/java/com/ingenieriadesoftware/facupdate/FacUpdate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ingenieriadesoftware.facupdate;
+
+/**
+ *
+ * @author USER
+ */
+public class FacUpdate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/FacUpdate/src/main/java/com/ingenieriadesoftware/facupdate/FacUpdate_1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ingenieriadesoftware.facupdate;
+import javax.swing.JFrame;
+
+import VistaLogin.java; 
+/**
+ *
+ * @author USER
+ */
+public class FacUpdate {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("FacUpdate Login");
+        frame.setSize(1000,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
